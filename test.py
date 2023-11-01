1dcc39a417401fd71e95f788997ca0eca8bcda9c
gongyi64 <124588451+gongyi64@users.noreply.github.com>
2023-11-01 14:44:03 +0900

    20231101さらに修正

--- test.py
+++ test.py
@@ -9,6 +9,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 import os
+import time
+
+options = webdriver.ChromeOptions()
+
 #from webdriver_manager.chrome import ChromeDriverManager
 
 #driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -32,17 +36,19 @@ import os
 
 driver_path = "C:\\Users\\406239\\AppData\\Local\\Programs\\Python\\Python39\\chromedriver_binary\\chromedriver.exe"
 
+print("========== kekka ========== 動いてる？")
 
-driver = webdriver.Chrome(service=ChromeService(driver_path))
 
 # service = Service(driver_path)
 # service = Service(executable_path=driver_path)# 2) executable_pathを指定
 # driver = webdriver.Chrome(service=service)# 3) serviceを渡す
 
 # 起動時にオプションをつける。（ポート指定により、起動済みのブラウザのドライバーを取得）
-options = webdriver.ChromeOptions()
+# options = webdriver.ChromeOptions()
 options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 # driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
+driver = webdriver.Chrome(service=ChromeService(driver_path))
 
 # driver = webdriver.Chrome(service,options=options)
 
@@ -51,6 +57,11 @@ options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
 get_title = driver.title
 print(get_title)
-print("========== kekka ========== ")
+print("========== kekka ========== 動いてる？")
 print(driver.page_source)
+
+time.sleep(5)
+
+driver.quit()
+
 
